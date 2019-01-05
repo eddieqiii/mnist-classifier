ddd9e3d829872b6c8949c85c469aeec0113493ee
Eddie Qi <eddieqi128@gmail.com>
2019-01-06 02:40:20 +1100

    cpu

--- graph.py
+++ graph.py
@@ -11,53 +11,53 @@ num_1 = 16
 num_2 = 16
 num_output = 10
 
-with tf.device('/device:GPU:0'):
-	with tf.variable_scope("input"):
-		input_nodes = tf.placeholder(np.float32, shape=(num_input, None))
+#with tf.device('/device:CPU:0'):
+with tf.variable_scope("input"):
+	input_nodes = tf.placeholder(np.float32, shape=(num_input, None))
 
-	with tf.variable_scope("layer1"):
-		layer1_weights = tf.get_variable("weights1", 
-			dtype=np.float32, 
-			shape=(num_1, num_input), 
-			initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
-		layer1_biases = tf.get_variable("biases1", 
-			dtype=np.float32, 
-			shape=(num_1, 1), 
-			initializer=tf.zeros_initializer())
-		layer1_nodes = tf.math.sigmoid(tf.matmul(layer1_weights, input_nodes)
-			+ layer1_biases)
+with tf.variable_scope("layer1"):
+	layer1_weights = tf.get_variable("weights1", 
+		dtype=np.float32, 
+		shape=(num_1, num_input), 
+		initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
+	layer1_biases = tf.get_variable("biases1", 
+		dtype=np.float32, 
+		shape=(num_1, 1), 
+		initializer=tf.zeros_initializer())
+	layer1_nodes = tf.math.sigmoid(tf.matmul(layer1_weights, input_nodes)
+		+ layer1_biases)
 
-	with tf.variable_scope("layer2"):
-		layer2_weights = tf.get_variable("weights2", 
-			dtype=np.float32, 
-			shape=(num_2, num_1), 
-			initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
-		layer2_biases = tf.get_variable("biases2", 
-			dtype=np.float32, 
-			shape=(num_2, 1), 
-			initializer=tf.zeros_initializer())
-		layer2_nodes = tf.math.sigmoid(tf.matmul(layer2_weights, layer1_nodes)
-			+ layer2_biases)
+with tf.variable_scope("layer2"):
+	layer2_weights = tf.get_variable("weights2", 
+		dtype=np.float32, 
+		shape=(num_2, num_1), 
+		initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
+	layer2_biases = tf.get_variable("biases2", 
+		dtype=np.float32, 
+		shape=(num_2, 1), 
+		initializer=tf.zeros_initializer())
+	layer2_nodes = tf.math.sigmoid(tf.matmul(layer2_weights, layer1_nodes)
+		+ layer2_biases)
 
-	with tf.variable_scope("output"):
-		output_weights = tf.get_variable("weightsout", 
-			dtype=np.float32, 
-			shape=(num_output, num_2), 
-			initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
-		output_biases = tf.get_variable("biasesout", 
-			dtype=np.float32, 
-			shape=(num_output, 1), 
-			initializer=tf.zeros_initializer())
-		output_nodes = tf.math.sigmoid(tf.matmul(output_weights, layer2_nodes)
-			+ output_biases)
+with tf.variable_scope("output"):
+	output_weights = tf.get_variable("weightsout", 
+		dtype=np.float32, 
+		shape=(num_output, num_2), 
+		initializer=tf.initializers.random_uniform(minval=-1, maxval=1))
+	output_biases = tf.get_variable("biasesout", 
+		dtype=np.float32, 
+		shape=(num_output, 1), 
+		initializer=tf.zeros_initializer())
+	output_nodes = tf.math.sigmoid(tf.matmul(output_weights, layer2_nodes)
+		+ output_biases)
 
-	with tf.variable_scope("cost"):
-		expected_output = tf.placeholder(np.float32, shape=(num_output, 1))
-		cost = tf.square(output_nodes - expected_output)
+with tf.variable_scope("cost"):
+	expected_output = tf.placeholder(np.float32, shape=(num_output, 1))
+	cost = tf.square(output_nodes - expected_output)
 
-	with tf.variable_scope("train"):
-		optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-		train = optimizer.minimize(cost)
+with tf.variable_scope("train"):
+	optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+	train = optimizer.minimize(cost)
 
 model_saver = tf.train.Saver()
 
